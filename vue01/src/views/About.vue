4efb674c205f737bc1a386f380538b4bf2b43856
tansizheng <tansizheng0606@163.com>
2022-08-04 17:59:19 +0800

    ok

--- vue01/src/views/About.vue
+++ vue01/src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+    <input type="text" v-model = aaa>
+    <div>
+      {{aaa}}
+    </div>
+</div>
   </div>
 </template>
+
+<script>
+export default {
+    data () {
+        return{
+            aaa: "123456"
+        }
+    }
+}
+</script>
